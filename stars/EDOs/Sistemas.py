1c105e51e0c0dd297ae1ae629d8ccd9b4d7a271a
Mandy8808 <arestrada88@gmail.com>
2024-06-26 07:18:33 -0600

    update

--- stars/EDOs/Sistemas.py
+++ stars/EDOs/Sistemas.py
@@ -1,26 +1,27 @@
 # Estrellas/EDOs
 
-def systemNS(r, yV, feps):
+def systemNS(r, yV, arg):
     """ 
     [g, g', N, N', p, p', \[Rho]] -> [g0, g1, n0, n1, p0, p1, rho]
     """
     g0, n0, p0 = yV
 
-    eps = feps(p0)
+    feps, = arg
+    # eps = feps(p0)  # warning
     if r==0:
-        f0, f1, f2 = 0, 0, 0
-    if p0<0:
+        f0, f1, f2 = 0., 0., 0.
+    elif p0<0:
         # Sch. solution
         f0 = (g0-g0**3)/(2.*r)
         f1 = (n0*(g0**2-1))/(2.*r)
         f2 = 0
     else:
+        eps = feps(p0)  # remove the warning
         f0 = (g0 + g0**3*(-1 + eps*r**2))/(2.*r)
         f1 = (n0*(-1 + g0**2*(1 + p0*r**2)))/(2.*r)
         
         n1 = f1
         f2 = -((n1*(eps + p0))/n0)
-    
     return [f0, f1, f2]
 
 
---
+++ stars/Utilez/constantes.py
@@ -0,0 +1,15 @@
+
+
+# FACTORES DE CONVERSION
+# recordar: dyne = 1e-5 Newton
+facDyneCmSqtoNm = 1e-01  # 1e-5/1e-4
+facGramCm3toKgmet = 1e03  # 1e-03/1e-06
+
+# constantes universales
+c = 299792458  # m/s
+hb = 1.054571817e-34  # kg m^2/s
+Mp = 2.17645e-08  # kg
+
+# datos del Sol
+Ms = 1.9884e30  # kg
+Rs = 6.957e+8 # m
